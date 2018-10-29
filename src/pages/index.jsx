7511ab8842843f6838ea421bbdc7591dd02684e2
Jack Davies <jack@noface.co.uk>
2018-10-29 21:48:52 +0000

    Remove fetch

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -60,24 +60,6 @@ const monthNames = [
 
 const d = new Date();
 
-var myCodepens = fetch("https://cpv2api.com/pens/popular/jackpritchard")
-	.then(function(response) {
-		if (response.status !== 200) {
-			console.log(
-				"Looks like there was a problem. Status Code: " + response.status
-			);
-			return;
-		}
-
-		// Examine the text in the response
-		response.json().then(function(data) {
-			console.log(data);
-		});
-	})
-	.catch(function(err) {
-		console.log("Fetch Error :-S", err);
-	});
-
 var myTimeline = [
 	{
 		title: "Freelance Front-end Developer",
