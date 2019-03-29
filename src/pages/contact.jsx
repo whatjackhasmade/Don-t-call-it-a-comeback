a00a97f671598804bd77925285f2bdf5a7abc558
Jack Pritchard <jack@noface.co.uk>
2019-03-29 16:51:32 +0000

    Render contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,13 +5,10 @@ import Base from "../components/templates/Base";
 import Contact from "../components/organisms/contact/Contact";
 import Hero from "../components/organisms/hero/Hero";
 
-class ContactPage extends Component {
+export default class ContactPage extends Component {
 	render() {
 		return (
 			<Base>
-				<Hero>
-					<h1>Contact</h1>
-				</Hero>
 				<Contact />
 			</Base>
 		);
