e79159429425734553642c1839553885e078984d
Jack Pritchard <jack@noface.co.uk>
2019-04-14 19:50:55 +0100

    Grid as stateless functional component

--- src/components/organisms/grid/Grid.jsx
+++ src/components/organisms/grid/Grid.jsx
@@ -1,83 +1,78 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import GridComponent from "./GridStyles";
 
-export default class Grid extends Component {
-	state = {
-		count: 100
+function Grid({ filter, items }) {
+	const [count, updateCount] = useState(100);
+
+	const addItems = () => {
+		updateCount(count + 9);
 	};
 
-	addItems = () => {
-		this.setState({ count: this.state.count + 9 });
-	};
+	var rows = [];
+	for (var i = 0; i < count; i++) {
+		const item = items[i];
 
-	render() {
-		const { filter, items } = this.props;
+		if (typeof item === "undefined") break;
 
-		var rows = [];
-		for (var i = 0; i < this.state.count; i++) {
-			const item = items[i];
+		const media = item.node.media;
+		const ext = media.substr(media.lastIndexOf(".") + 1);
 
-			if (typeof item === "undefined") break;
-
-			const media = item.node.media;
-			const ext = media.substr(media.lastIndexOf(".") + 1);
-
-			if (ext === `mp4`) {
-				rows.push(
-					<GridItem category={item.node.category} filter={filter}>
-						<video src={item.node.media} alt="" autoPlay muted loop />
-						<span className="grid__item__title">{item.node.title}</span>
-					</GridItem>
-				);
-			} else {
-				rows.push(
-					<GridItem category={item.node.category} filter={filter}>
-						<img src={item.node.media} alt="" />
-						<span className="grid__item__title">{item.node.title}</span>
-					</GridItem>
-				);
-			}
-		}
-
-		return (
-			<>
-				<GridComponent>{rows}</GridComponent>
-				<button
-					disabled={this.state.count > this.props.items.length}
-					onClick={this.addItems}
+		if (ext === `mp4`) {
+			rows.push(
+				<GridItem
+					key={`grid-item-${i}`}
+					category={item.node.category}
+					filter={filter}
 				>
-					Load More
-				</button>
-			</>
-		);
-	}
-}
-
-class GridItem extends Component {
-	state = {
-		fullScreen: false
-	};
-
-	toggleFullscreen = e => {
-		this.setState({ fullScreen: !this.state.fullScreen });
-	};
-
-	render() {
-		const { category, filter } = this.props;
-
-		let classList = `grid__item`;
-		if (this.state.fullScreen) classList += ` grid__item--fullscreen`;
-
-		if (filter !== ``) {
-			if (category === filter) classList += ` grid__item--active`;
-			if (category !== filter) classList += ` grid__item--inactive`;
+					<video src={item.node.media} alt="" autoPlay muted loop />
+					<span className="grid__item__title">{item.node.title}</span>
+				</GridItem>
+			);
+		} else {
+			rows.push(
+				<GridItem
+					key={`grid-item-${i}`}
+					category={item.node.category}
+					filter={filter}
+				>
+					<img src={item.node.media} alt="" />
+					<span className="grid__item__title">{item.node.title}</span>
+				</GridItem>
+			);
 		}
-
-		return (
-			<div className={classList} onClick={this.toggleFullscreen}>
-				{this.props.children}
-			</div>
-		);
 	}
+
+	return (
+		<>
+			<GridComponent>{rows}</GridComponent>
+			<button disabled={count > items.length} onClick={addItems}>
+				Load More
+			</button>
+		</>
+	);
 }
+
+function GridItem({ category, children, filter }) {
+	const [fullScreen, changeFullScreen] = useState(false);
+
+	const toggleFullscreen = e => {
+		changeFullScreen(!fullScreen);
+	};
+
+	let classList = `grid__item`;
+	if (fullScreen) classList += ` grid__item--fullscreen`;
+
+	if (filter !== ``) {
+		if (category === filter) classList += ` grid__item--active`;
+		if (category !== filter) classList += ` grid__item--inactive`;
+	}
+
+	return (
+		<div className={classList} onClick={toggleFullscreen}>
+			{children}
+		</div>
+	);
+}
+
+export default Grid;
