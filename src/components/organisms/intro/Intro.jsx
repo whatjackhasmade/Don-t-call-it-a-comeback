f2ffbc053de262855b57226992106630e3646581
Jack Pritchard <jack@noface.co.uk>
2019-04-14 19:51:14 +0100

    Intro as stateless functional component

--- src/components/organisms/intro/Intro.jsx
+++ src/components/organisms/intro/Intro.jsx
@@ -2,46 +2,43 @@ import React, { Component } from "react";
 
 import IntroComponent from "./IntroStyles";
 
-export default class Intro extends Component {
-	render() {
-		const { data, heading, index, marginReduced, subheading } = this.props;
+function Intro(props) {
+	const { data, heading, index, marginReduced, subheading } = props;
+	const Subheading = index === 0 ? `h1` : `h2`;
+	const Heading = index === 0 ? `h2` : `h3`;
 
-		const Subheading = index === 0 ? `h1` : `h2`;
-		const Heading = index === 0 ? `h2` : `h3`;
-
-		if (data) {
-			return (
-				<IntroComponent
-					className="intro"
-					marginReduced={marginReduced ? false : true}
-				>
-					<div className="intro__wrapper">
-						<Subheading className="h4 intro__subheading">
-							{data.subheading}
-						</Subheading>
-						<Heading className="h1 intro__heading">{data.heading}</Heading>
-						<div
-							className="intro__contents"
-							dangerouslySetInnerHTML={{ __html: data.content }}
-						/>
-					</div>
-				</IntroComponent>
-			);
-		} else {
-			return (
-				<IntroComponent
-					className="intro"
-					marginReduced={marginReduced ? false : true}
-				>
-					<div className="intro__wrapper">
-						<Subheading className="h4 intro__subheading">
-							{subheading}
-						</Subheading>
-						<Heading className="h1 intro__heading">{heading}</Heading>
-						<div className="intro__contents">{this.props.children}</div>
-					</div>
-				</IntroComponent>
-			);
-		}
+	if (data) {
+		return (
+			<IntroComponent
+				className="intro"
+				marginReduced={marginReduced ? false : true}
+			>
+				<div className="intro__wrapper">
+					<Subheading className="h4 intro__subheading">
+						{data.subheading}
+					</Subheading>
+					<Heading className="h1 intro__heading">{data.heading}</Heading>
+					<div
+						className="intro__contents"
+						dangerouslySetInnerHTML={{ __html: data.content }}
+					/>
+				</div>
+			</IntroComponent>
+		);
+	} else {
+		return (
+			<IntroComponent
+				className="intro"
+				marginReduced={marginReduced ? false : true}
+			>
+				<div className="intro__wrapper">
+					<Subheading className="h4 intro__subheading">{subheading}</Subheading>
+					<Heading className="h1 intro__heading">{heading}</Heading>
+					<div className="intro__contents">{props.children}</div>
+				</div>
+			</IntroComponent>
+		);
 	}
 }
+
+export default Intro;
