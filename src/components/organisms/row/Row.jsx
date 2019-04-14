674d3867d1966d1ad5d1ebac0889d46a257a618e
Jack Pritchard <jack@noface.co.uk>
2019-04-14 16:59:45 +0100

    Convert Row component to function component

--- src/components/organisms/row/Row.jsx
+++ src/components/organisms/row/Row.jsx
@@ -1,51 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
 import { InView } from "react-intersection-observer";
-import { useSpring, animated } from "react-spring";
+// import { useSpring, animated } from "react-spring";
 
 import RowComponent from "./RowStyles";
 
 import ImageLoader from "../../molecules/imageloader/ImageLoader";
 
-export default class Row extends Component {
-	prepareContent = content => {
+function Row(props) {
+	const { data, index } = props;
+	const { content, link, media } = data;
+
+	const alignment = index % 2 === 0 ? `left` : `right`;
+
+	const mediaFull = media.full;
+	const ext = mediaFull.substr(mediaFull.lastIndexOf(".") + 1);
+
+	const prepareContent = content => {
 		return content.replace(
 			`/wp-content/uploads/`,
 			`https://wjhm.noface.app/wp-content/uploads/`
 		);
 	};
 
-	render() {
-		const { data, index } = this.props;
-		const { content, link, media } = data;
-
-		const alignment = index % 2 === 0 ? `left` : `right`;
-
-		const mediaFull = media.full;
-		const ext = mediaFull.substr(mediaFull.lastIndexOf(".") + 1);
-
-		return (
-			<InView threshold={0.25} triggerOnce={true}>
-				{({ inView, ref }) => (
-					<RowComponent
-						className={
-							inView
-								? `row row--${alignment} row--show`
-								: `row row--${alignment}`
-						}
-						ref={ref}
-					>
-						<div className="row__column">
-							{link !== "" ? (
-								<a href={`${link}`} target="_blank" rel="noopener noreferrer">
-									<div className="row__media">
-										{ext !== `mp4` ? (
-											<ImageLoader src={mediaFull} alt="" />
-										) : (
-											<video src={mediaFull} muted autoPlay loop />
-										)}
-									</div>
-								</a>
-							) : (
+	return (
+		<InView threshold={0.25} triggerOnce={true}>
+			{({ inView, ref }) => (
+				<RowComponent
+					className={
+						inView ? `row row--${alignment} row--show` : `row row--${alignment}`
+					}
+					ref={ref}
+				>
+					<div className="row__column">
+						{link !== "" ? (
+							<a href={`${link}`} target="_blank" rel="noopener noreferrer">
 								<div className="row__media">
 									{ext !== `mp4` ? (
 										<ImageLoader src={mediaFull} alt="" />
@@ -53,17 +41,27 @@ export default class Row extends Component {
 										<video src={mediaFull} muted autoPlay loop />
 									)}
 								</div>
-							)}
-						</div>
-						<div
-							className="row__column row__content"
-							dangerouslySetInnerHTML={{
-								__html: this.prepareContent(content)
-							}}
-						/>
-					</RowComponent>
-				)}
-			</InView>
-		);
-	}
+							</a>
+						) : (
+							<div className="row__media">
+								{ext !== `mp4` ? (
+									<ImageLoader src={mediaFull} alt="" />
+								) : (
+									<video src={mediaFull} muted autoPlay loop />
+								)}
+							</div>
+						)}
+					</div>
+					<div
+						className="row__column row__content"
+						dangerouslySetInnerHTML={{
+							__html: prepareContent(content)
+						}}
+					/>
+				</RowComponent>
+			)}
+		</InView>
+	);
 }
+
+export default Row;
