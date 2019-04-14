74ab70184e8235275a23306d02ba738bfacac356
Jack Pritchard <jack@noface.co.uk>
2019-04-14 19:50:40 +0100

    Footer component as stateless functional component

--- src/components/organisms/footer/Footer.jsx
+++ src/components/organisms/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 import FooterComponent from "./FooterStyles";
 
@@ -7,42 +7,42 @@ import IconLinkedIn from "../../../assets/images/icons/brands/linkedin.svg";
 import IconTwitter from "../../../assets/images/icons/brands/twitter.svg";
 import IconYouTube from "../../../assets/images/icons/brands/youtube.svg";
 
-export default class Footer extends Component {
-	render() {
-		return (
-			<FooterComponent>
-				<div className="footer__contents">
-					<nav className="footer__cta">
-						<a href="mailto:jack@noface.co.uk">
-							<div className="footer__arrow" />
-							<div className="footer__cta__content">
-								<span className="footer__tagline">
-									Always available for a chat
-								</span>
-								<span className="footer__name">Jack Pritchard</span>
-							</div>
-						</a>
-					</nav>
-					<nav className="footer__contact">
-						<a href="tel:07393 357520">07393 357520</a>
-						<a href="mailto:jack@noface.co.uk">jack@noface.co.uk</a>
-					</nav>
-					<nav className="footer__social">
-						<a href="https://twitter.com/whatjackhasmade">
-							<IconTwitter /> Twitter
-						</a>
-						<a href="https://github.com/whatjackhasmade">
-							<IconGithub /> Github
-						</a>
-						<a href="https://linkedin.com/in/whatjackhasmade">
-							<IconLinkedIn /> LinkedIn
-						</a>
-						<a href="https://youtube.com/whatjackhasmade">
-							<IconYouTube /> YouTube
-						</a>
-					</nav>
-				</div>
-			</FooterComponent>
-		);
-	}
+function Footer() {
+	return (
+		<FooterComponent>
+			<div className="footer__contents">
+				<nav className="footer__cta">
+					<a href="mailto:jack@noface.co.uk">
+						<div className="footer__arrow" />
+						<div className="footer__cta__content">
+							<span className="footer__tagline">
+								Always available for a chat
+							</span>
+							<span className="footer__name">Jack Pritchard</span>
+						</div>
+					</a>
+				</nav>
+				<nav className="footer__contact">
+					<a href="tel:07393 357520">07393 357520</a>
+					<a href="mailto:jack@noface.co.uk">jack@noface.co.uk</a>
+				</nav>
+				<nav className="footer__social">
+					<a href="https://twitter.com/whatjackhasmade">
+						<IconTwitter /> Twitter
+					</a>
+					<a href="https://github.com/whatjackhasmade">
+						<IconGithub /> Github
+					</a>
+					<a href="https://linkedin.com/in/whatjackhasmade">
+						<IconLinkedIn /> LinkedIn
+					</a>
+					<a href="https://youtube.com/whatjackhasmade">
+						<IconYouTube /> YouTube
+					</a>
+				</nav>
+			</div>
+		</FooterComponent>
+	);
 }
+
+export default Footer;
