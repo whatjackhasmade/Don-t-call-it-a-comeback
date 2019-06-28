b23c51258e63db06c56809001e294010e5f14b9a
Jack Pritchard <jack@noface.co.uk>
2019-06-28 08:15:34 +0100

    Fix 3

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,3 +3,18 @@ const path = require("path");
 const _ = require("lodash");
 const moment = require("moment");
 const siteConfig = require("./data/SiteConfig");
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+	if (stage === "build-html") {
+		actions.setWebpackConfig({
+			module: {
+				rules: [
+					{
+						test: /mailtoui/,
+						use: loaders.null()
+					}
+				]
+			}
+		});
+	}
+};
